d3c5da93244b3127fe6962dbbed1ff2cb4b7a83f
leon.jankovic <leon.jankovic@fer.hr>
2023-11-04 15:58:17 +0100

    Basic player, targeting system

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,8 @@
+/*
+ * po jedna skripta za svakog igraca,
+ * svaki igrac moze gledat svoj hp, attack power, status effects, abilities i sta cemo sve dodat
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +10,40 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public int _NUMBER;
+    public int _MAXHP = 20;
+    public int hp;
+
+    public int ability_block = 0;
+
+    private int target;
     void Awake()
     {
-        
+        hp = _MAXHP;
+    }
+    private void Update()
+    {
+        target = GameObject.Find("Main Camera").GetComponent<Setup>().target;
+        if (hp > _MAXHP) hp = _MAXHP;
+    }
+
+    public virtual void Ability_Basic()
+    {
+        GameObject.Find("Main Camera").GetComponent<Setup>().players[target].GetComponent<PlayerScript>().hp--;         //nisam trijezan dok ovo pisem, ak neko moze malo bolje / ljepse slobodno
+    }
+
+    public virtual void Ability_1()
+    {
+        ++hp;
+    }
+
+    public virtual void Ability_2()
+    {
+        GameObject.Find("Main Camera").GetComponent<Setup>().players[target].GetComponent<PlayerScript>().ability_block++;
+    }
+
+    public void turn_end()
+    {
+        if(ability_block > 0) ability_block--;
     }
 
 }
--- Assets/Scripts/Setup.cs
+++ Assets/Scripts/Setup.cs
@@ -1,16 +1,26 @@
+/*
+ * ovu skriptu koristimo za setup, praæenje poteza, kad se kome pali HUD i slicno
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Setup : MonoBehaviour
 {
-    public GameObject player;
+    private Camera currentCamera;
+    private bool hotseat;
 
-    private int _CURRENT_TURN;
+    public GameObject player;
+    public int turn_player = 0;
     private int _MAX_PLAYERS = 8;
     public int playerCount;
+    public int target = -1;
+    public GameObject[] players;
     void Awake()
     {
+        hotseat = true;
+
         if (playerCount > _MAX_PLAYERS) playerCount = _MAX_PLAYERS;
         SpawnPlayers();
     }
@@ -18,9 +28,44 @@ public class Setup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (!currentCamera)
+        {
+            currentCamera = Camera.main;
+            return;
+        }
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out info, Mathf.Infinity, LayerMask.GetMask("Player")))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                target = info.transform.gameObject.GetComponent<PlayerScript>()._NUMBER;
+                if (target == turn_player) target = -1;
+                else Debug.Log("TARGET: " + target);
+            }
+        }
 
+        //OVO DOLE MAKNUT KAD SE NAPRAVI HUD, TRENUTNO NEMA CHECK JEL LEGALNO ISPUCAT ABILITY
+        if (Input.GetKeyDown("1"))
+        {
+            players[turn_player].GetComponent<PlayerScript>().Ability_Basic();
+            players[turn_player].GetComponent<PlayerScript>().turn_end();
+            next_turn();
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            players[turn_player].GetComponent<PlayerScript>().Ability_1();
+            players[turn_player].GetComponent<PlayerScript>().turn_end();
+            next_turn();
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            players[turn_player].GetComponent<PlayerScript>().Ability_2();
+            players[turn_player].GetComponent<PlayerScript>().turn_end();
+            next_turn();
+        }
+
+    }
     void SpawnPlayers()
     {
         Vector2 direction = new Vector2(0, -4);
@@ -30,13 +75,23 @@ public class Setup : MonoBehaviour
         for (int i = 0; i < playerCount; i++)
         {
             GameObject p = Instantiate(player);
-            p.name = "Player " + (i + 1);
+            p.name = "Player " + (i);
             Vector3 pos = Quaternion.AngleAxis(angle * i, axis) * direction;
             pos.x = pos.z;      //x i z se mijesaju a ja neznam zasto
             pos.z = 0;
             //Debug.Log(pos);
             p.transform.position = pos;
             p.GetComponent<PlayerScript>()._NUMBER = i;
+
+            players[i] = p;
         }
+    }
+
+    public void next_turn()
+    {
+        players[turn_player].GetComponent<PlayerScript>().turn_end();
+        if (++turn_player >= _MAX_PLAYERS) turn_player = 0;
+        if (hotseat) target = -1;
+        Debug.Log("TURN PLAYER: " + turn_player);
     }
 }
